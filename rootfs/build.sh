7e4e9953bea75f22908501284ee17f9d54a257cc
Tien Tran <tien@transfamily.info>
2021-08-03 07:42:51 +1000

    Fix additional docker groups

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -32,7 +32,7 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update && apt install -y docker-ce docker-ce-cli containerd.io
-for gid in 497 998; do addgroup -g $gid docker$gid; addgroup node docker$gid; done
+for gid in 497 998; do addgroup --gid $gid docker$gid; adduser node docker$gid; done
 
 # install python3 + pip
 apt install -y python3-pip

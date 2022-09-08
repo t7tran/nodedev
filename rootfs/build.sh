209bad2b179228d1fe4ae783b510d87b0c1ce9cc
Tien Tran <tien@transfamily.info>
2022-09-08 12:48:58 +1000

    Install git-lfs extension

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -7,7 +7,7 @@ sed -i 's/bullseye main/bullseye main contrib/g' /etc/apt/sources.list
 apt update && apt upgrade -y && apt autoremove -y
 
 # apk add git curl ncurses mc dpkg hstr
-apt install -y vim git curl mc jq dpkg iputils-ping
+apt install -y vim git git-lfs curl mc jq dpkg iputils-ping
 # libncurses5-dev libncursesw5-dev # was needed by hstr???
 
 # install hstr

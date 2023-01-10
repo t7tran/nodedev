84168fd2b712410928b96f1014e09e0ce86dcca1
Tien Tran <tien@transfamily.info>
2023-01-10 13:15:50 +1100

    Upgrade gosu v1.6

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -22,7 +22,7 @@ apt install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libn
 
 # install gosu
 dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
-curl -fsSL "https://github.com/tianon/gosu/releases/download/1.14/gosu-$dpkgArch" -o /usr/local/bin/gosu
+curl -fsSL "https://github.com/tianon/gosu/releases/download/1.16/gosu-$dpkgArch" -o /usr/local/bin/gosu
 chmod +x /usr/local/bin/gosu
 gosu nobody true
 

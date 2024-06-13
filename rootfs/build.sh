8912045baeeadf1e43a6239d86b803ce36c9943a
Tien Tran <tien@transfamily.info>
2024-06-13 16:40:06 +1000

    Install tmate for instant terminal sharing

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -86,9 +86,10 @@ yarn global add npm
 # install pnpm
 yarn global add pnpm
 
-# install ngrok & localtunnel
+# install ngrok, localtunnel, and tmate
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | tee /etc/apt/sources.list.d/ngrok.list && apt update && apt install ngrok
 yarn global add localtunnel
+apt install -y tmate
 
 # install packages globally
 yarn global add \

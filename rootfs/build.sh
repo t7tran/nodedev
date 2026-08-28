95e97bcc0faa411869dcaad0025bf4df956ec168
Tien Tran <tien@transfamily.info>
2026-08-28 14:34:56 +1000

    Fix gcloud cli installation

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -143,7 +143,7 @@ if [[ "$VARIANT" != "slim" ]]; then
   # install gcloud SDK -----------------------------------------------------
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-  apt update && apt install -y google-cloud-sdk google-cloud-sdk-gke-gcloud-auth-plugin
+  apt update && apt install -y google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin
   gosu node gcloud config set core/disable_usage_reporting true
   gosu node gcloud config set component_manager/disable_update_check true
   gosu node gcloud config set metrics/environment github_docker_image
